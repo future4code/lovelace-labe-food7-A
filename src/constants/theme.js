611eadda0f7beec9cbe10b6cae81af610ab32383
Ingrid FLack <ingrid_flack@hotmail.com>
2021-09-07 12:45:48 -0300

    estilizacao dos detalhes da tela de login

--- src/constants/theme.js
+++ src/constants/theme.js
@@ -1,16 +1,24 @@
-import { createTheme } from '@material-ui/core/styles';
-import { primaryColor, neutralColor } from './colors'
+import { createTheme } from "@material-ui/core/styles";
+import { primaryColor, neutralColor } from "./colors";
 
 const theme = createTheme({
-    palette: {
-        primary: {
-            main: primaryColor,
-            contrastText: 'black'
-        },
-        text: {
-            primary: neutralColor
-        }
-    }
-})
+  palette: {
+    primary: {
+      main: primaryColor,
+      contrastText: "black",
+    },
+    text: {
+      primary: neutralColor,
+    },
+  },
+  overrides: {
+    MuiButton: {
+      root: {
+        textTransform: "none",
+        fontSize: "16px",
+      },
+    },
+  },
+});
 
 export default theme;
